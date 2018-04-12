769255f2420ee630b09e7121313ad892c2c5b2ad
lexuantien1997 <tienlx97@gmail.com>
2018-04-12 13:14:40 +0700

    load product into check-out.html

--- src/check-out.js
+++ src/check-out.js
@@ -3,18 +3,12 @@ $(document).ready(function(){
     var data_array = []
 
     var loadDataFromSession2Table = function () {
-        // console.log(sessionStorage);
+         console.log(sessionStorage);
         Object.keys(sessionStorage).map(function(c,i,a){
             var cartValue = sessionStorage.getItem(c);
              var item = JSON.parse( cartValue );
              if (item !== true)
-                $('#myTable').append("<tr><td>"+ item.name +"</td><td>"+item.price+"</td><td>"+ item.quality +"</td><td>"+item.price*item.quality+"</td></tr>");
-           data_array.push(item);
-            // // do whatever
-            // console.log(c)
-            // console.log(i)
-            // console.log(a)
-            // console.log("===============================================")
+                $('#mytable').append("<tr><td class='col-md-5'><img src=" + item.urlImage+" width='100' height='100' style = 'margin-right:10px; margin-bottom:10px'>"+ item.name +"</td><td class='col-md-2'>"+item.price+"</td><td class='col-md-2'>"+ item.quality +"</td><td class='col-md-3'>"+item.price*item.quality+"</td></tr>");
           });
 
           console.log(data_array);
